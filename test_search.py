cbea32fb952492e9b986bca76f8a3511f509ac96
gazizovaa <gazizova12047@ada.edu.az>
2024-03-11 17:46:56 +0400

    Try to lcate the search input in a correct way

--- test_search.py
+++ test_search.py
@@ -1,23 +1,26 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 import time
 
 
 class DribbleWebsiteSearchTest(unittest.TestCase):
     def setUp(self):
         self.browser_driver = webdriver.Chrome()
-        self.browser_driver.implicitly_wait(30)
-        self.browser_driver.get("https://dribbble.com/")
+        self.browser_driver.implicitly_wait(10)
+        self.browser_driver.get("https://dribbble.com/search")
 
-    def test_search_box(self):
-        search_query = "design"
-        search_input = self.browser_driver.find_element_by_name("q")
-        search_input.send_keys(search_query + Keys.RETURN)
-        time.sleep(2)
-
-        get_search_results = self.browser_driver.find_elements_by_class_name("shot-thumbnail")
-        self.assertGreater(len(get_search_results), 0)
+    def test_search_app_design(self):
+        search_design = "chat app ui"
+        search_input = WebDriverWait(self.browser_driver, 10).until(EC.element_to_be_clickable((By.ID, "search")))
+        search_input.send_keys(search_design)
+        time.sleep(5)
+        search_input.send_keys(Keys.RETURN)
+        time.sleep(5)
+        assert search_design in self.browser_driver.page_source
 
     def tearDown(self):
         self.browser_driver.quit()
